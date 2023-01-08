fbfcf62db0b92cc5f3c9197f9f5f347f3b54ebaa
Drew Pereli <drewpereli@gmail.com>
2023-01-08 00:30:27 -0800

    Add cors

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import config from './config';
+import cors from 'cors';
 
 import todoRoutes from './routes/todo-routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: [/^https?:\/\/localhost:\d+$/],
+  })
+);
 
 app.use('/api/v1/todos', todoRoutes);
 
