d5570e1f0eb646863b7ed846b6035eb1694f34d0
Drew Pereli <drewpereli@gmail.com>
2023-01-15 23:10:38 -0800

    Make it a progressive web app

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,8 +1,47 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { VitePWA } from 'vite-plugin-pwa';
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
+import fs from 'node:fs/promises';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      manifest: {
+        short_name: 'YATD',
+        name: 'Yet Another Todo List',
+        theme_color: '#ffffff',
+        start_url: '/',
+        icons: [
+          {
+            src: 'images/logo_192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'images/logo_512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+    vitePluginFaviconsInject('./public/images/logo.png'),
+    {
+      name: 'remove-favicons-manifest',
+      transformIndexHtml(html) {
+        return html.replace(
+          '<link rel="manifest" href="/assets/manifest.webmanifest">',
+          ''
+        );
+      },
+    },
+  ],
   server: {
     port: 3000,
   },
